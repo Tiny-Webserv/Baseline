65ac276e0533e509970a26ef3c5290585ba3502f
SaltySalt77 <alexiena@naver.com>
2023-05-08 17:01:11 +0900

    feat : 특정 컨텐트 타입 처리 완료

--- srcs/Request.cpp
+++ srcs/Request.cpp
@@ -11,23 +11,13 @@ Request::Request() : _chunked(false) {}
 
 Request::Request(int fd, std::stringstream &stream)
     : _contentLength(-1), _errorMessages("OK"), _errorCode(OK), _chunked(false), _hostPort(80), _isEnd(false) {
-    // std::vector<std::string> splited = Split2(stream.str(), CRLF);
 
     try {
-        int i = 0;
-        std::cout << i++ << std::endl;
         std::string buff;
         std::getline(stream, buff, '\n');
-		std::cout << "=====================\n" << "startline and header : " << stream.str() << std::endl;
         setStartLine(buff);
-        std::cout << i++ << std::endl;
         setHeader(stream.str());
-        std::cout << i++ << std::endl;
-        // std::vector<std::string>::iterator iter = splited.begin() + 3;
-        // SetBody(iter);
         readBody(fd);
-        //std::cout << i++ << std::endl;
-		std::cout << "body : " << _stream.str() << std::endl;
         splitHost();
 		if (_contentType == "multipart/form-data")
 			parseFormData();
@@ -39,21 +29,18 @@ Request::Request(int fd, std::stringstream &stream)
 }
 
 Request::Request(std::vector<char>	&formData, std::string	delimeter){
-	//write(2, &formData[0], formData.size());
-	(void) delimeter;
 	std::string	headerStr(formData.begin(), formData.end());
 	std::string doubleCRLF = CRLF;
 	doubleCRLF.append(CRLF);
-	size_t delimeterPos = headerStr.find(doubleCRLF);
+	size_t delimeterPos = headerStr.find(delimeter);
 	if (delimeterPos == std::string::npos)
 		return ;
     setHeader(headerStr.substr(0, delimeterPos));
-	formData.erase(formData.begin(), formData.begin() + headerStr.find(doubleCRLF));
+	if (headerStr.find(doubleCRLF) == std::string::npos)
+		return ;
+	formData.erase(formData.begin(), formData.begin() + headerStr.find(doubleCRLF) + doubleCRLF.size());
     std::copy(formData.begin(), find(formData.begin(), formData.end(), '\r'),
               std::back_inserter(_binary));
-	std::cerr << std::endl << " ======================================&&&&&&&& start " << std::endl;
-	write(2, &_binary[0], _binary.size());
-	std::cerr << " ======================================&&&&&&&& end " << std::endl;
 	formData.erase(formData.begin(), find(formData.begin(), formData.end(), '\r') + 2);
 }
 
@@ -62,7 +49,6 @@ Request::~Request() {
 		for (size_t i = 0; i < _formData.size(); i++)
 			delete _formData[i];
     }
-	std::cout << "!!!!!!!!!!!!!!!!die" << std::endl;
 }
 
 void Request::SetMethod(std::string method) { _method = method; }
@@ -126,7 +112,6 @@ Request::Request(const Request &request)
 
 void Request::setStartLine(std::string startLine) {
     std::vector<std::string> data = Split(startLine, std::string(" "));
-    std::cout << "startLine : " << startLine << std::endl;
     if (!data[0].compare("GET") || !data[0].compare("POST") ||
         !data[0].compare("DELETE"))
         _method = data[0];
@@ -135,19 +120,15 @@ void Request::setStartLine(std::string startLine) {
     _target = Split(data[1], std::string(":"))[0]; //[0]은 vector 0번째 인덱스 접근을 위함
     std::cout << data[2] << std::endl;
     // std::cout << std::isstring(data[2]) << std::endl;
-
     if (data[2].compare("HTTP/1.1\r"))
         throw HTTPVersionError();
 }
 
 void Request::setHeader(std::string header) {
-    std::cerr << "this is header " << header << std::endl;
     std::vector<std::string> splited = Split(header, " \r\n;");
     std::vector<std::string>::iterator iter;
     for (std::vector<std::string>::iterator i = splited.begin();
          i != splited.end(); i++) {
-        std::cout << "what is splited ? " << *i << std::endl;
-        /* code */
     }
 
     iter = find(splited.begin(), splited.end(), "Content-Type:");
@@ -174,7 +155,6 @@ void Request::setHeader(std::string header) {
 				_fileName = (iter + i)->substr(std::string("filename=").size());
 				_fileName.erase(0, 1);
 				_fileName.erase(_fileName.size() - 1, 1);
-				std::cerr << "filename : " << _fileName << std::endl;
 				//break ;
             }
 		}
@@ -186,8 +166,6 @@ void Request::splitHost() {
     _hostName = splited[0]; // name
     if (splited.size() > 1)
         _hostPort = atoi(splited[1].c_str());
-    std::cout << "hostName : " << _hostName << std::endl;
-    std::cout << "hostPort :" << _hostPort << std::endl;
 }
 
 std::string Request::GetHostName() { return this->_hostName; }
@@ -201,8 +179,6 @@ void Request::readBody(int fd) {
     int valRead;
     char buffer[1024];
 
-    std::cout << " 1: ==> " << std::endl;
-
     if (_chunked) {
         line = get_next_line(fd);
         line = line.substr(0, line.size() - CRLF_SIZE);
@@ -214,18 +190,12 @@ void Request::readBody(int fd) {
         }
         _contentLength = static_cast<int>(x);
     }
-    std::cout << " 2: ==> " << std::endl;
-
     int j = 0;
     while ((valRead = recv(fd, buffer, 1024, 0)) > 0) {
         for (int i = 0; i < valRead; i++)
             _binary.push_back(buffer[i]);
         std::cout << j++ << std::endl;
     }
-    std::cout << " 3: ==> " << std::endl;
-
-    std::cout << "valRead : " << valRead << std::endl;
-    std::cout << "errno : " << errno << std::endl;
     // CRLF가 2번인지 확인해서 다 읽었는지 아닌지 확인
     if (!_chunked && _binary.size()) {
         size_t end = _binary.size() - 1;
@@ -241,31 +211,22 @@ void Request::readBody(int fd) {
         //예외 처리
         _isEnd = false;
     }
-	std::cout << "==========================" << std::endl;
-	write(1, &_binary[0], _binary.size());
-	std::cout << "==========================" << std::endl;
 }
 
 void Request::parseFormData() {
 	size_t i = 0;
-	write(2, &_binary[0], _binary.size());
-	std::cerr << std::endl;
 	while (_boundary[i] == '-') i++;
 	_boundary.erase(0, i);
 	i = 0;
 	while (1) {
 		i = 0;
-		std::cerr << _boundary << std::endl;
 		while (i < _binary.size() && _binary[i] == '-') i++;
 		_binary.erase(_binary.begin(), _binary.begin() + i);
 		i = 0;
 		while (i < _binary.size() && _binary[i] == _boundary[i]) i++;
-		_binary.erase(_binary.begin(), _binary.begin() + i);
+		_binary.erase(_binary.begin(), _binary.begin() + i) + 2;
 		if (_binary.size() == 0 || _binary[0] == '-')
 			break ;
-		//write(2, &_binary[0], _binary.size());
 		_formData.push_back(new Request(_binary, _boundary));
 	}
-	//_binary.erase(_binary.begin(), _binary.end() - 1);
-	//write(2, &_binary[0], _binary.size());
 }
