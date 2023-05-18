71e7623a2601baec35599f82d466446ecbaa845f
wkddlstn1321 <wkddlstn132@gmail.com>
2023-05-18 12:57:56 +0900

    fix:delete php

--- srcs/Php.cpp
+++ srcs/Php.cpp
@@ -286,6 +286,8 @@
 //}
 
 bool IsPhp(Request *reque) {
+    if (reque->GetMethod() == "DELETE")
+        return false;
     std::string target = reque->GetTarget();
     // get일때 본문이 있으면 url 로 들어와서 짤라줌
     if (reque->GetMethod() == "GET" && target.find('?') != std::string::npos)
