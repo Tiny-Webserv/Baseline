f63059bbb8936b1e481826ef7a0d1e0ccb9cb4fe
Devicii <62472550+asroq1@users.noreply.github.com>
2023-05-22 15:58:00 +0900

    fix : isdirect 조건문 수정

--- srcs/http/Response.cpp
+++ srcs/http/Response.cpp
@@ -291,7 +291,8 @@ bool Response::isRedirect() {
     redirectLocation.insert(redirectLocation.end(), tmp.begin(), tmp.end());
     std::string str(redirectLocation.begin(), redirectLocation.end());
     _redirectLocation = str;
-    if (getLocationBlock().GetReturn().first == 302) {
+    
+    if (getLocationBlock().GetReturn().first == 302 && _request->GetTarget() == getLocationBlock().GetReturn().second) {
         _request->SetErrorCode(302);
         _request->SetErrorMessages("Moved Temporarily");
         _isRedirection = true;
